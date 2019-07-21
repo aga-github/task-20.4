ca9336d5b7d86c280cf0c7b864b5703d60fc0eed
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-07-21 19:30:48 +0200

    Add containers and change files for task-20.5

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
-import DevTools from './DevTools';
+//import DevTools from './DevTools';
+import './country.css'
 
 render(
     <Provider store={store}>
         <div>
             <Router history={hashHistory} routes={routes}/>
-            <DevTools/>
         </div>
     </Provider>,
     document.getElementById('root')
